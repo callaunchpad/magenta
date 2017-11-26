985af808ace7a7274152ff91cd899ea8efb9e09e
Arsh Zahed <arshzahed@gmail.com>
2017-11-25 20:32:20 -0800

    jk not i did

--- magenta/models/shared/pfnn.py
+++ magenta/models/shared/pfnn.py
@@ -144,159 +144,159 @@ class PhaseAttentionCellWrapper(AttentionCellWrapper):
 
     def __init__(self, cell, attn_length, attn_size=None, attn_vec_size=None,
                input_size=None, state_is_tuple=True, reuse=None):
-    """Create a cell with attention.
-    Args:
-      cell: an RNNCell, an attention is added to it.
-      attn_length: integer, the size of an attention window.
-      attn_size: integer, the size of an attention vector. Equal to
-          cell.output_size by default.
-      attn_vec_size: integer, the number of convolutional features calculated
-          on attention state and a size of the hidden layer built from
-          base cell state. Equal attn_size to by default.
-      input_size: integer, the size of a hidden linear layer,
-          built from inputs and attention. Derived from the input tensor
-          by default.
-      state_is_tuple: If True, accepted and returned states are n-tuples, where
-        `n = len(cells)`.  By default (False), the states are all
-        concatenated along the column axis.
-      reuse: (optional) Python boolean describing whether to reuse variables
-        in an existing scope.  If not `True`, and the existing scope already has
-        the given variables, an error is raised.
-    Raises:
-      TypeError: if cell is not an RNNCell.
-      ValueError: if cell returns a state tuple but the flag
-          `state_is_tuple` is `False` or if attn_length is zero or less.
-    """
-    super(AttentionCellWrapper, self).__init__(_reuse=reuse)
-    if not rnn_cell_impl._like_rnncell(cell):  # pylint: disable=protected-access
-        raise TypeError("The parameter cell is not RNNCell.")
-    if nest.is_sequence(cell.state_size) and not state_is_tuple:
-        raise ValueError("Cell returns tuple of states, but the flag "
-                       "state_is_tuple is not set. State size is: %s"
-                       % str(cell.state_size))
-    if attn_length <= 0:
-        raise ValueError("attn_length should be greater than zero, got %s"
-                       % str(attn_length))
-    if not state_is_tuple:
-        logging.warn(
-          "%s: Using a concatenated state is slower and will soon be "
-          "deprecated.  Use state_is_tuple=True.", self)
-    if attn_size is None:
-        attn_size = cell.output_size
-    if attn_vec_size is None:
-        attn_vec_size = attn_size
-    self._state_is_tuple = state_is_tuple
-    self._cell = cell
-    self._attn_vec_size = attn_vec_size
-    self._input_size = input_size -1  # discount phase
-    self._attn_size = attn_size
-    self._attn_length = attn_length
-    self._reuse = reuse
-    self._linear1 = None
-    self._linear2 = None
-    self._linear3 = None
-    self.phase = None
+        """Create a cell with attention.
+        Args:
+          cell: an RNNCell, an attention is added to it.
+          attn_length: integer, the size of an attention window.
+          attn_size: integer, the size of an attention vector. Equal to
+              cell.output_size by default.
+          attn_vec_size: integer, the number of convolutional features calculated
+              on attention state and a size of the hidden layer built from
+              base cell state. Equal attn_size to by default.
+          input_size: integer, the size of a hidden linear layer,
+              built from inputs and attention. Derived from the input tensor
+              by default.
+          state_is_tuple: If True, accepted and returned states are n-tuples, where
+            `n = len(cells)`.  By default (False), the states are all
+            concatenated along the column axis.
+          reuse: (optional) Python boolean describing whether to reuse variables
+            in an existing scope.  If not `True`, and the existing scope already has
+            the given variables, an error is raised.
+        Raises:
+          TypeError: if cell is not an RNNCell.
+          ValueError: if cell returns a state tuple but the flag
+              `state_is_tuple` is `False` or if attn_length is zero or less.
+        """
+        super(AttentionCellWrapper, self).__init__(_reuse=reuse)
+        if not rnn_cell_impl._like_rnncell(cell):  # pylint: disable=protected-access
+            raise TypeError("The parameter cell is not RNNCell.")
+        if nest.is_sequence(cell.state_size) and not state_is_tuple:
+            raise ValueError("Cell returns tuple of states, but the flag "
+                           "state_is_tuple is not set. State size is: %s"
+                           % str(cell.state_size))
+        if attn_length <= 0:
+            raise ValueError("attn_length should be greater than zero, got %s"
+                           % str(attn_length))
+        if not state_is_tuple:
+            logging.warn(
+              "%s: Using a concatenated state is slower and will soon be "
+              "deprecated.  Use state_is_tuple=True.", self)
+        if attn_size is None:
+            attn_size = cell.output_size
+        if attn_vec_size is None:
+            attn_vec_size = attn_size
+        self._state_is_tuple = state_is_tuple
+        self._cell = cell
+        self._attn_vec_size = attn_vec_size
+        self._input_size = input_size -1  # discount phase
+        self._attn_size = attn_size
+        self._attn_length = attn_length
+        self._reuse = reuse
+        self._linear1 = None
+        self._linear2 = None
+        self._linear3 = None
+        self.phase = None
 
     def call(self, inputs, state):
-    """Long short-term memory cell with attention (LSTMA)."""
+        """Long short-term memory cell with attention (LSTMA)."""
 
-    # store phase, shorten inputs
-    self.phase = inputs[-1]
-    inputs = inputs[:-1]
+        # store phase, shorten inputs
+        self.phase = inputs[-1]
+        inputs = inputs[:-1]
 
-    if self._state_is_tuple:
-        state, attns, attn_states = state
-    else:
-        states = state
-        state = array_ops.slice(states, [0, 0], [-1, self._cell.state_size])
-        attns = array_ops.slice(
-          states, [0, self._cell.state_size], [-1, self._attn_size])
-        attn_states = array_ops.slice(
-          states, [0, self._cell.state_size + self._attn_size],
-          [-1, self._attn_size * self._attn_length])
-    attn_states = array_ops.reshape(attn_states,
-                                    [-1, self._attn_length, self._attn_size])
-    input_size = self._input_size
-    if input_size is None:
-        input_size = inputs.get_shape().as_list()[1]
-    if self._linear1 is None:
-        self._linear1 = _Linear([inputs, attns], input_size, True)
+        if self._state_is_tuple:
+            state, attns, attn_states = state
+        else:
+            states = state
+            state = array_ops.slice(states, [0, 0], [-1, self._cell.state_size])
+            attns = array_ops.slice(
+              states, [0, self._cell.state_size], [-1, self._attn_size])
+            attn_states = array_ops.slice(
+              states, [0, self._cell.state_size + self._attn_size],
+              [-1, self._attn_size * self._attn_length])
+        attn_states = array_ops.reshape(attn_states,
+                                        [-1, self._attn_length, self._attn_size])
+        input_size = self._input_size
+        if input_size is None:
+            input_size = inputs.get_shape().as_list()[1]
+        if self._linear1 is None:
+            self._linear1 = _Linear([inputs, attns], input_size, True)
 
-    inputs = self._linear1([inputs, attns])
+        inputs = self._linear1([inputs, attns])
 
-    # append phase back into input so that PFNN can use it
-    inputs.append(self.phase)
+        # append phase back into input so that PFNN can use it
+        inputs.append(self.phase)
 
-    cell_output, new_state = self._cell(inputs, state)
-    if self._state_is_tuple:
-        new_state_cat = array_ops.concat(nest.flatten(new_state), 1)
-    else:
-        new_state_cat = new_state
-    new_attns, new_attn_states = self._attention(new_state_cat, attn_states)
-    with vs.variable_scope("attn_output_projection"):
-        if self._linear2 is None:
-            self._linear2 = _Linear([cell_output, new_attns], self._attn_size, True)
-        output = self._linear2([cell_output, new_attns])
-    new_attn_states = array_ops.concat(
-        [new_attn_states, array_ops.expand_dims(output, 1)], 1)
-    new_attn_states = array_ops.reshape(
-        new_attn_states, [-1, self._attn_length * self._attn_size])
-    new_state = (new_state, new_attns, new_attn_states)
-    if not self._state_is_tuple:
-        new_state = array_ops.concat(list(new_state), 1)
+        cell_output, new_state = self._cell(inputs, state)
+        if self._state_is_tuple:
+            new_state_cat = array_ops.concat(nest.flatten(new_state), 1)
+        else:
+            new_state_cat = new_state
+        new_attns, new_attn_states = self._attention(new_state_cat, attn_states)
+        with vs.variable_scope("attn_output_projection"):
+            if self._linear2 is None:
+                self._linear2 = _Linear([cell_output, new_attns], self._attn_size, True)
+            output = self._linear2([cell_output, new_attns])
+        new_attn_states = array_ops.concat(
+            [new_attn_states, array_ops.expand_dims(output, 1)], 1)
+        new_attn_states = array_ops.reshape(
+            new_attn_states, [-1, self._attn_length * self._attn_size])
+        new_state = (new_state, new_attns, new_attn_states)
+        if not self._state_is_tuple:
+            new_state = array_ops.concat(list(new_state), 1)
 
-    return output, new_state
+        return output, new_state
 
 class PhaseDropoutWrapper(DropoutWrapper):
-  """Operator adding dropout to inputs and outputs of the given cell.
+    """Operator adding dropout to inputs and outputs of the given cell.
      Incorporates phase. """
 
     def __init__(self, cell, input_keep_prob=1.0, output_keep_prob=1.0,
-    state_keep_prob=1.0, variational_recurrent=False,
-    input_size=None, dtype=None, seed=None,
-    dropout_state_filter_visitor=None):
+        state_keep_prob=1.0, variational_recurrent=False,
+        input_size=None, dtype=None, seed=None,
+        dropout_state_filter_visitor=None):
 
-    super().__init__(self, cell, input_keep_prob, output_keep_prob, state_keep_prob, variational_recurrent,
-        input_size, dtype, seed, dropout_state_filter_visitor)
+        super().__init__(self, cell, input_keep_prob, output_keep_prob, state_keep_prob, variational_recurrent,
+            input_size, dtype, seed, dropout_state_filter_visitor)
 
-    #don't know if I need this
-    if input_size:
-        self.input_size = input_size -1
+        #don't know if I need this
+        if input_size:
+            self.input_size = input_size -1
 
-    self.phase = None
-    return
+        self.phase = None
+        return
 
     def __call__(self, inputs, state, scope=None):
-    """Run the cell with the declared dropouts."""
+        """Run the cell with the declared dropouts."""
 
-    # store phase value
-    self.phase = inputs[-1]
-    inputs = inputs[:-1]
+        # store phase value
+        self.phase = inputs[-1]
+        inputs = inputs[:-1]
 
-    def _should_dropout(p):
-         return (not isinstance(p, float)) or p < 1
+        def _should_dropout(p):
+             return (not isinstance(p, float)) or p < 1
 
-    if _should_dropout(self._input_keep_prob):
-        inputs = self._dropout(inputs, "input",
-                             self._recurrent_input_noise,
-                             self._input_keep_prob)
+        if _should_dropout(self._input_keep_prob):
+            inputs = self._dropout(inputs, "input",
+                                 self._recurrent_input_noise,
+                                 self._input_keep_prob)
 
-    # re-append phase so PFNN can use it
-    inputs.append(self.phase)
+        # re-append phase so PFNN can use it
+        inputs.append(self.phase)
 
-    output, new_state = self._cell(inputs, state, scope)
+        output, new_state = self._cell(inputs, state, scope)
 
-    if _should_dropout(self._state_keep_prob):
-    #       Identify which subsets of the state to perform dropout on and
-      # which ones to keep.
-        shallow_filtered_substructure = nest.get_traverse_shallow_structure(
-                                self._dropout_state_filter, new_state)
-        new_state = self._dropout(new_state, "state",
-                                self._recurrent_state_noise,
-                                self._state_keep_prob,
-                                shallow_filtered_substructure)
-    if _should_dropout(self._output_keep_prob):
-        output = self._dropout(output, "output",
-                             self._recurrent_output_noise,
-                             self._output_keep_prob)
-    return output, new_state
+        if _should_dropout(self._state_keep_prob):
+        #       Identify which subsets of the state to perform dropout on and
+          # which ones to keep.
+            shallow_filtered_substructure = nest.get_traverse_shallow_structure(
+                                    self._dropout_state_filter, new_state)
+            new_state = self._dropout(new_state, "state",
+                                    self._recurrent_state_noise,
+                                    self._state_keep_prob,
+                                    shallow_filtered_substructure)
+        if _should_dropout(self._output_keep_prob):
+            output = self._dropout(output, "output",
+                                 self._recurrent_output_noise,
+                                 self._output_keep_prob)
+        return output, new_state
